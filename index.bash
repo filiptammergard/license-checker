15756ec68f749b76803b16ba49608eaf8b072db8
Filip Tammergård <filip@tammergard.se>
2022-01-05 12:33:53 +0100

    fix: temporarily comment our stuff

--- index.bash
+++ index.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "checking licenses..."
-npx --yes license-checker --summary --excludePrivatePackages --failOn 'UNLICENCED;GPL-1.0-or-later;LGPL-2.0-or-later;AGPL-3.0;MS-PL;SPL-1.0;CC-BY-NC-1.0;CC-BY-NC-2.0;CC-BY-NC-2.5;CC-BY-NC-4.0;CC-BY-NC-ND-1.0;CC-BY-NC-ND-2.0;CC-BY-NC-ND-2.5;CC-BY-NC-ND-4.0;CC-BY-NC-SA-1.0;CC-BY-NC-SA-2.0;CC-BY-NC-SA-2.5;CC-BY-NC-SA-4.0;EUPL-1.0;EUPL-1.1;EUPL-1.2'
+# npx --yes license-checker --summary --excludePrivatePackages --failOn 'UNLICENCED;GPL-1.0-or-later;LGPL-2.0-or-later;AGPL-3.0;MS-PL;SPL-1.0;CC-BY-NC-1.0;CC-BY-NC-2.0;CC-BY-NC-2.5;CC-BY-NC-4.0;CC-BY-NC-ND-1.0;CC-BY-NC-ND-2.0;CC-BY-NC-ND-2.5;CC-BY-NC-ND-4.0;CC-BY-NC-SA-1.0;CC-BY-NC-SA-2.0;CC-BY-NC-SA-2.5;CC-BY-NC-SA-4.0;EUPL-1.0;EUPL-1.1;EUPL-1.2'
